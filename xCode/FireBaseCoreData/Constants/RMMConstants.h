c8450e788d4edd49670aed2296e5962b4f2bc97a
Ricardo Maqueda <rinconestolmo@gmail.com>
2014-09-10 18:40:56 +0200

    Añadir Constants file. Again!

---
+++ xCode/FireBaseCoreData/Constants/RMMConstants.h
@@ -0,0 +1,14 @@
+//..
+//  RMMConstants.h
+//  FireBaseCoreData
+//
+//  Created by Ricardo Maqueda on 08/09/14.
+//  Copyright (c) 2014 Molestudio. All rights reserved.
+//
+
+#ifndef FireBaseCoreData_RMMConstants_h
+#define FireBaseCoreData_RMMConstants_h
+
+#define kFireBaseURL @"https://YOUR_APP_HERE.firebaseio.com/coreData"
+
+#endif
